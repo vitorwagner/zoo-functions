18117d859d43f8eb19956866584a511e38fbbf50
Vitor Wagner <vitormwagner@gmail.com>
2022-09-29 15:01:26 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui2
+function getSpeciesByIds(...ids) {
+  return species.filter(({ id }) => ids.includes(id));
 }
 
 module.exports = getSpeciesByIds;
