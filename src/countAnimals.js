669b7f06e9e4e6a785ad783abdcb97f8f326613c
Vitor Wagner <vitormwagner@gmail.com>
2022-09-29 16:56:06 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+// Referência utilizada: https://stackoverflow.com/questions/64188099/how-to-reduce-array-of-objects-into-one-object
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, creature) => (
+      { ...acc, [creature.name]: creature.residents.length }
+    ), {});
+  }
+  const animalName = species.find((creature) => creature.name === animal.specie);
+  if (!animal.sex) {
+    return animalName.residents.length;
+  }
+  return animalName.residents.filter((creature) => creature.sex === animal.sex).length;
 }
-
 module.exports = countAnimals;
