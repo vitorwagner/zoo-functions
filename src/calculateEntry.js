23e0e573dc3287ec2f5af35f1c2ee04204c92a68
Vitor Wagner <vitormwagner@gmail.com>
2022-09-29 17:24:18 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+// Referência: https://www.freecodecamp.org/news/how-to-count-objects-in-an-array/
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const count = {
+    child: entrants.filter((item) => item.age < 18).length || 0,
+    adult: entrants.filter((item) => item.age >= 18 && item.age < 50).length || 0,
+    senior: entrants.filter((item) => item.age >= 50).length || 0,
+  };
+
+  return count;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  return count.child * prices.child + count.adult * prices.adult + count.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
