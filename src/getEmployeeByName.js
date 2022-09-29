7131a5bdcb7dffc23de5102775c8376d0074a4d9
Vitor Wagner <vitormwagner@gmail.com>
2022-09-29 15:16:14 -0300

    Correção Lint Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,7 @@ const { employees } = require('../data/zoo_data');
 // Referência para o return usando OR: https://eloquentjavascript.net/01_values.html#i_3jN0iK4yKW
 
 function getEmployeeByName(employeeName) {
-  const gotEmployee = employees.find((({ firstName, lastName }) => firstName === employeeName 
+  const gotEmployee = employees.find((({ firstName, lastName }) => firstName === employeeName
   || lastName === employeeName));
   return gotEmployee || {};
 }
