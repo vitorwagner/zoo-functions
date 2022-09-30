89fce97c5328942feefa22aa05a11b724045a78c
Vitor Wagner <vitormwagner@gmail.com>
2022-09-30 16:37:20 -0300

    Requisito 14 :rocket:

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getSpeciesMap() {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach(({ name, location }) => map[location].push(name));
+  return map;
 }
 
+function getAnimalList(animalName, options = { includeNames: false, sorted: false, sex: false }) {
+  let list = species.find(({ name }) => name === animalName).residents;
+  if (options.sex) list = list.filter((creature) => options.sex === creature.sex);
+  return options.sorted ? list.map(({ name }) => name).sort() : list.map(({ name }) => name);
+}
+
+function getNamedMap(options) {
+  const map = { NE: [], NW: [], SE: [], SW: [] };
+  species.forEach(({ name, location }) => map[location]
+    .push({ [name]: getAnimalList(name, options) }));
+  return map;
+}
+
+function getAnimalMap(options = { includeNames: false, sorted: false, sex: false }) {
+  if (!options.includeNames) return getSpeciesMap();
+  return getNamedMap(options);
+}
 module.exports = getAnimalMap;
